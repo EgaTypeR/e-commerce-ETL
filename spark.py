360f970675abb179c2400625bcd2dc24776f3175
EgaTypeR <ega.rizky.setiawan@mail.ugm.ac.id>
2024-10-22 08:48:12 +0700

    add: grouping

--- spark.py
+++ spark.py
@@ -1,11 +1,19 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import sum
 
 spark = SparkSession.builder.appName('Sales-ETL').getOrCreate()
 # Load the dataset
-data_path = "/sales.xlsx"  # Adjust this path to your dataset
+data_path = "./sales.csv"  # Adjust this path to your dataset
 
 # Load the Excel file
 sales_df = spark.read.csv(data_path, header=True, inferSchema=True)
 
-# Show the dataset
-sales_df.show()
+# Calculate total sales, quantity, and profit by region
+sales_summary_by_region = sales_df.groupBy("Region").agg(
+    sum("Sales").alias("Total_Sales"),
+    sum("Quantity").alias("Total_Quantity"),
+    sum("Profit").alias("Total_Profit")
+)
+
+# Show the result
+sales_summary_by_region.show()
