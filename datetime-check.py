96ceb0d232775697cb7d71b88a98de1edf70870b
EgaTypeR <ega.rizky.setiawan@mail.ugm.ac.id>
2024-10-26 00:37:47 +0700

    fix date check file

--- datetime-check.py
+++ datetime-check.py
@@ -11,11 +11,13 @@ spark = SparkSession.builder \
         .config("spark.jars.packages", "org.postgresql:postgresql:42.7.4") \
         .getOrCreate()
 
+spark.conf.set("spark.sql.legacy.timeParserPolicy", "LEGACY")
+
 sales_df = spark.read.option("header", "true").csv(data_path, inferSchema=True)
 sales_df = sales_df.toDF(*[col_name.strip() for col_name in sales_df.columns])
 print(sales_df.head())
 print("------\n")
-sales_df = sales_df.withColumn("Date", to_date(date_format(col("Date"), "yyyy-MM-dd"), "yyyy-MM-dd"))
+sales_df = sales_df.withColumn("Date", to_date(col("Date"), "MM/dd/yyyy"))
 print(sales_df.head())
 # Check for null dates
 null_count = sales_df.filter(col("Date").isNull()).count()
